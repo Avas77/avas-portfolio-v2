97684c45402d8a751046708ee7a6007113e526ca
Avas <avas.bajracharya@renegadeinsurance.com>
2024-03-26 09:48:06 +0545

    feat: added theme colors and cleanup code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      background: "#282C33",
+      primary: "#C778DD",
+      gray: "#ABB2BF",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
