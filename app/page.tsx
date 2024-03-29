7be69afa7c4d9ba22e431d05b9be65d55a8987b4
Avas <avas.bajracharya@renegadeinsurance.com>
2024-03-29 10:28:50 +0545

    feat: added responsive styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <main className="flex h-[423px] justify-between items-center mt-[62px]">
-      <section className="basis-full">
+    <main className="flex flex-col sm:flex-row h-auto sm:h-[423px] justify-between items-center mt-[34px] sm:mt-[62px] gap-6 sm:gap-0">
+      <section className="basis-full px-4 sm:px-6 lg:px-0">
         <h1 className="font-semibold text-[32px]">
           Crafting seamless digital experiences - Meet Avas, your&nbsp;
           <strong className="text-primary font-semibold">
             full stack web development expert
           </strong>
         </h1>
-        <p className="mt-8 text-gray">
+        <p className="mt-[25px] sm:mt-8 text-gray">
           I make websites that load quickly and work well on all devices. I
           focus on making them easy for people to use and enjoy
         </p>
@@ -18,7 +18,7 @@ const Home = () => {
           Contact me!!
         </button>
       </section>
-      <section className="basis-9/12">
+      <section className="basis-9/12 px-[22px] lg:px-0">
         <div className="relative">
           <Image
             src={"/assets/purple-line.svg"}
@@ -42,9 +42,9 @@ const Home = () => {
             className="absolute right-4 z-20 bottom-14"
           />
         </div>
-        <div className="flex items-center gap-[10px] border border-gray px-2 py-2 w-11/12">
+        <div className="flex flex-wrap items-center gap-2 sm:gap-[10px] border border-gray px-2 py-2 w-auto sm:w-11/12">
           <div className="bg-primary w-4 h-4" aria-hidden="true"></div>
-          <span className="text-gray before:">Currently working on </span>
+          <span className="text-gray">Currently working on </span>
           <strong className="font-semibold">Portfolio</strong>
         </div>
       </section>
