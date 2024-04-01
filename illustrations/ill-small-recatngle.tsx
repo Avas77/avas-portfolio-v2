b34abafb9639ea71644d34200b23b561f76bfcff
Avas <avas.bajracharya@renegadeinsurance.com>
2024-04-01 12:30:52 +0545

    feat: added side shapes

---
+++ illustrations/ill-small-recatngle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ILLSmallRectangle = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 82 91"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect x="0.5" y="0.5" width="90" height="90" stroke="#ABB2BF" />
+    </svg>
+  );
+};
+
+export default ILLSmallRectangle;
