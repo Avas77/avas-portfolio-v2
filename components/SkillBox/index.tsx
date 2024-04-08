5b4393c9ffc0e5a7b090c3bfe4dc875966c40a9e
Avas <avas.bajracharya@renegadeinsurance.com>
2024-04-08 21:13:31 +0545

    feat: added components style layer

--- components/SkillBox/index.tsx
+++ components/SkillBox/index.tsx
@@ -8,9 +8,7 @@ interface ISKillBoxProps {
 
 const SkillBox = ({ groupName, skills, classNames }: ISKillBoxProps) => {
   return (
-    <section
-      className={`${classNames} lg:[&:nth-child(4)]:col-start-3 lg:[&:nth-child(4)]:col-end-4 lg:[&:nth-child(5)]:col-start-2 lg:[&:nth-child(5)]:col-end-3 lg:[&:nth-child(5)]:row-start-2 lg:[&:nth-child(5)]:row-end-3 lg:[&:nth-child(4)]:mt-[73px]`}
-    >
+    <section className={`${classNames} skillBox`}>
       <h4 className="font-semibold border border-white p-2">{groupName}</h4>
       <p className="text-gray border border-white p-2">{skills}</p>
     </section>
