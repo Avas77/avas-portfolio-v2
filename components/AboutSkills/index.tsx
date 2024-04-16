9bc0c37fe710d25ec953db67a7a23b7913354721
Avas <avas.bajracharya@renegadeinsurance.com>
2024-04-16 19:41:01 +0545

    feat: added responsive styles for about page

--- components/AboutSkills/index.tsx
+++ components/AboutSkills/index.tsx
@@ -5,10 +5,10 @@ import SkillsList from "../SkillsList";
 const AboutSkills = () => {
   return (
     <section className="flex flex-col mb-[83px]">
-      <BlockHeader classNames="hiddne" title="skills" />
+      <BlockHeader classNames="hidden" title="skills" />
       <SkillsList
         classNames="flex flex-wrap gap-4 mt-12"
-        boxClassNames="w-[192px]"
+        boxClassNames="w-full md:w-[192px]"
       />
     </section>
   );
