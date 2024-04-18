2fdbab6ece7ae875cfedebaaf1c103bf367a86cf
Avas <avas.bajracharya@renegadeinsurance.com>
2024-04-18 16:35:48 +0545

    feat: added responsive styles

--- components/ContactsBox/index.tsx
+++ components/ContactsBox/index.tsx
@@ -7,7 +7,7 @@ interface IContactsBox {
 
 const ContactsBox = ({ contents, title }: IContactsBox) => {
   return (
-    <section className="border border-white p-4 w-fit h-fit">
+    <section className="border border-white p-4 w-full h-full md:w-fit md:h-fit">
       <h6 className="font-semibold mb-4">{title}</h6>
       <div className="flex flex-col gap-2">{contents}</div>
     </section>
