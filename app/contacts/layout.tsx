7b1b37b8f822e4f68fe61aa95bdcaed6aed9e087
Avas <avas.bajracharya@renegadeinsurance.com>
2024-04-19 16:33:03 +0545

    feat: added shapes in contacts page

--- app/contacts/layout.tsx
+++ app/contacts/layout.tsx
@@ -1,5 +1,8 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import SideShapes from "@/components/SIdeShapes";
+import SideLinks from "@/components/SideLinks";
+import Image from "next/image";
 import React from "react";
 
 const Contactslayout = ({
@@ -9,6 +12,23 @@ const Contactslayout = ({
 }>) => {
   return (
     <>
+      <SideLinks />
+      <SideShapes classNames="left-[-7px] top-[401px]">
+        <Image
+          src="/assets/small-dots.png"
+          width={109}
+          height={49}
+          alt="dots"
+        />
+      </SideShapes>
+      <SideShapes classNames="right-[-80px] top-64">
+        <Image
+          src={"/assets/small-rectangle.svg"}
+          alt="rectangle"
+          width={155}
+          height={155}
+        />
+      </SideShapes>
       <section className="container">
         <Header />
         {children}
