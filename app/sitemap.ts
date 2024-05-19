dc718225caf226bfbf55363fc01aeb1806c91d28
Avas <avas.bajracharya@renegadeinsurance.com>
2024-05-19 17:54:07 +0545

    feat: added missing blog links

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
   const blogRoutes = allBlogs.map((post) => ({
-    url: `${siteUrl}/blog/${post.slug}`,
+    url: `${siteUrl}/blogs/${post.slug}`,
     lastModified: post.publishedAt,
   }));
   const routes = pageRoutes.map((route) => ({
@@ -15,3 +15,4 @@ export default function sitemap(): MetadataRoute.Sitemap {
   }));
   return [...routes, ...blogRoutes];
 }
+  
