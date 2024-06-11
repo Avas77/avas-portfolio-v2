dc46879127f8c9c121d9fe02b566aa3ad81912f7
Avas <avas.bajracharya@renegadeinsurance.com>
2024-06-11 12:26:34 +0545

    feat: added metadata for blogs page

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 
 const space_grotesk = Space_Grotesk({
@@ -7,6 +8,12 @@ const space_grotesk = Space_Grotesk({
   display: "swap",
   variable: "--font-space-grotesk",
 });
+
+export const metadata: Metadata = {
+  title: "Blogs",
+  description:
+    "Welcome to the Technical Blogs of Avas Bajracharya, a software engineer specializing in front-end development. Explore insightful articles, tutorials, and the latest industry trends designed to help developers enhance their skills and stay ahead in the ever-evolving tech landscape. Join the community and dive into topics ranging from coding best practices to advanced software architectures.",
+};
 
 export default function BlogLayout({
   children, // will be a page or nested layout
