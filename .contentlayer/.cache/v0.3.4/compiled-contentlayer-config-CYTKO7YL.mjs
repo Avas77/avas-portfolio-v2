373bb837e9df7842294e33322edf0631562cfe99
Avas Bajracharya <insanebuzz09@gmail.com>
2026-07-23 18:44:21 -0400

    feat: added featured articles section and combined all articles

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-CYTKO7YL.mjs
@@ -0,0 +1,56 @@
+// contentlayer.config.ts
+import {
+  defineDocumentType,
+  makeSource
+} from "contentlayer/source-files";
+var computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").pop()
+  }
+};
+var Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    publishedAt: {
+      type: "string",
+      required: true
+    },
+    summary: {
+      type: "string",
+      required: true
+    },
+    image: {
+      type: "string"
+    },
+    series: {
+      type: "string",
+      required: false
+    },
+    order: {
+      type: "number",
+      required: false
+    },
+    seriesTitle: {
+      type: "string",
+      required: false
+    },
+    draft: { type: "boolean", default: false }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content/",
+  documentTypes: [Blog]
+});
+export {
+  Blog,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-CYTKO7YL.mjs.map
